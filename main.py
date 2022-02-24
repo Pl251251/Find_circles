56150d584c3682c2fd61f7df09d94725bd00f7c2
Pl251251 <91552726+Pl251251@users.noreply.github.com>
2022-02-24 11:45:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from PIL import Image
 import math
 import numpy as np
 #pillow
-im = Image.open('test_l3.jpg')
+im = Image.open('test_l3.jpeg')
 
 w,h =im.size
 im2 = Image.new(im.mode, (2*w, 2*h), (255,255,255))
 im2.paste(im, (int(w/2), int(h/2)))
 
 im_rotate = im2.rotate(348)
-im_rotate.save("test.jpg")
+im_rotate.save("test.jpg", quality = 100)
 #opencv
 img = cv2.imread('test.jpg', 1)
 width = img.shape[1]
@@ -21,20 +21,32 @@ resized_ = cv2.resize(img, (x_1, height))
 #finding a circle
 blur= cv2.medianBlur(resized_,7)
 gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
-circles = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=100,maxRadius=120)
-info = np.uint16(np.around(circles))
-a=0
-for i in info[0,:]:
-    cv2.circle(resized_,(i[0],i[1]),i[2],(255,0,0),2)
-    cv2.circle(resized_,(i[0],i[1]),2,(0,0,255),3)
+try:
+    circles = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=246,maxRadius=265)
+    info = np.uint16(np.around(circles))
+    a=400
 
-resized2_ = cv2.resize(resized_,(width, height))
+    for i in info[0,:]:
+        if i[2] < a:
+            a = i[2]
+            b= i
+    cv2.circle(resized_,(b[0],b[1]),b[2],(0,0,255),2)
+    cv2.circle(resized_,(b[0],b[1]),2,(0,0,255),3)
 
-cv2.imwrite("test.jpg", resized2_)
+except:
+    pass
+
+
+cv2.imshow('oval', resized_)
+cv2.waitKey(5000)
+cv2.destroyAllWindows()
+resized_ = cv2.resize(resized_,(width, height))
+
+cv2.imwrite("test.jpg", resized_, [cv2.IMWRITE_PNG_COMPRESSION, 0])
 #pillow
 im = Image.open('test.jpg')
 im_rotate = im.rotate(12)
-im_rotate.save("test.jpg")
+im_rotate.save("test.jpg", quality =100)
 
 #opencv
 img = cv2.imread('test.jpg',1)
@@ -45,8 +57,12 @@ dim4 = int((dim2/2) -h/2)+1
 dim5 = int((dim1/2) + w/2)-1
 dim6 = int((dim2/2) +h/2)-1
 img = img[dim4:dim6,dim3:dim5]
-cv2.imwrite("final.jpg", img)
+cv2.imwrite("final.jpg", img, [cv2.IMWRITE_PNG_COMPRESSION, 0])
 
 cv2.imshow('oval', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+im = Image.open('final.jpg')
+
+im_rotate.save("test.jpg", quality =100)
